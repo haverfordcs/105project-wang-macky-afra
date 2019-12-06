378ce30ee1c0ba860137cdb9719088836bceaad5
Caspian <caspianmacky@gmail.com>
2019-12-06 13:39:29 -0500

    Added lowercase support for orientation

--- placement.py
+++ placement.py
@@ -3,6 +3,7 @@
 import definingMatrices as dM
 import random
 playerBoard = ([[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']])
+enemyBoard = ([[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']])
 
 
 def ship_placement_player(matrix):  # This function runs through the ship-placement sequence for the player. Call it
@@ -19,13 +20,13 @@ def ship_placement_player(matrix):  # This function runs through the ship-placem
                 dM.printBoard(matrix)
                 print("You are placing your {}. It is {} squares long.".format(ship[0], ship[1]))  # Tell the player which ship they're placing and how long it is
                 print("Would you like to place your ship vertically or horizontally?")  # ask player if they want it horizontal or vertical
-                orientation = input("Input 'Vertical' or 'Horizontal': ")
-                if orientation == 'Vertical' or orientation == 'Horizontal':
-                    square = input("Enter the coordinates of the top-left square you want your ship to occupy in the form 'A1': ")  # ask player if they want it horizontal or vertical
+                orientation = input("Input 'Vertical' or 'Horizontal': ").lower()
+                if orientation == 'vertical' or orientation == 'horizontal':
+                    square = input("Enter the coordinates of the top-left square you want your ship to occupy in the form 'A0': ")  # ask player if they want it horizontal or vertical
                     if check_valid_coordinates(square):
                         if placement_is_valid(orientation, square, ship[1], matrix, "Player"):
                             matrix, player_ship_location_dict = place_friendly_ship(orientation, square, ship[1], matrix, ship[0], player_ship_location_dict)
-                            print("You have placed your {} {}ly starting on square {}".format(ship[0], orientation.lower(), square))
+                            print("You have placed your {} {}ly starting on square {}".format(ship[0], orientation, square))
                             placed = True
                     else:
                         print("That's not a valid entry.")
@@ -52,20 +53,20 @@ def ship_placement_ai(matrix):  # This function runs through the ship-placement 
                 # print("Would you like to place your ship vertically or horizontally?")  # ask player if they want it horizontal or vertical
                 # orientation = input("Input 'Vertical' or 'Horizontal': ")
                 orientationDict = {0: "Vertical", 1: "Horizontal"}
-                orientation = orientationDict[random.randint(0, 1)]
+                orientation = orientationDict[random.randint(0, 1)].lower()
                 # square = input("Enter the coordinates of the top-left square you want your ship to occupy in the form 'A1': ")  # ask player if they want it horizontal or vertical
                 columnDict = {0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F", 6: "G", 7: "H", 8: "I", 9: "J"}
                 square = ""+str(columnDict[random.randint(0, 9)])+str(random.randint(0, 9))
                 if placement_is_valid(orientation, square, ship[1], matrix, "AI"):
                     good_idea = False
-                    if orientation == "Vertical" and (orientation, coordinate_converter(square)[1]) not in bad_ideas:
+                    if orientation == "vertical" and (orientation, coordinate_converter(square)[1]) not in bad_ideas:
                         good_idea = True
-                    elif orientation == "Horizontal" and (orientation, coordinate_converter(square)[0]) not in bad_ideas:
+                    elif orientation == "horizontal" and (orientation, coordinate_converter(square)[0]) not in bad_ideas:
                         good_idea = True
                     if good_idea:
                         matrix, ai_ship_location_dict = place_enemy_ship(orientation, square, ship[1], matrix, ship[0], ai_ship_location_dict)
                         # print("The AI has placed their {} {}ly starting on square {}".format(ship[0], orientation.lower(), square))
-                        if orientation == "Vertical":
+                        if orientation == "vertical":
                             bad_ideas.append((orientation, coordinate_converter(square)[1]))
                             bad_ideas.append((orientation, coordinate_converter(square)[1]-1))
                             bad_ideas.append((orientation, coordinate_converter(square)[1]+1))
@@ -103,7 +104,7 @@ def placement_is_valid(orientation, square, length, matrix, identity):  # Checks
 
 def boundary_valid(orientation, square, length, identity):  # Function that checks if the potential placement goes off the board.
     # print("Checking boundary validity...")
-    if orientation == 'Vertical':
+    if orientation == 'vertical':
         if coordinate_converter(square)[0] + length < 11:
             # print("Placement is on board")
             return True
@@ -111,7 +112,7 @@ def boundary_valid(orientation, square, length, identity):  # Function that chec
             if identity == "Player":
                 print("Ship goes beyond bottom of matrix. Placement is invalid.")
             return False
-    elif orientation == 'Horizontal':
+    elif orientation == 'horizontal':
         if coordinate_converter(square)[1] + length < 11:
             # print("Placement is on board")
             return True
@@ -127,7 +128,7 @@ def boundary_valid(orientation, square, length, identity):  # Function that chec
 def overlap_valid(orientation, square, length, matrix, identity):  # Function that checks if the potential placement overlaps any other ships.
     cell = coordinate_converter(square)
     # print("Checking overlap validity...")
-    if orientation == 'Vertical':
+    if orientation == 'vertical':
         for i in range(0, length):
             if matrix[cell[0]+i][cell[1]] in "OI":
                 if identity == "Player":
@@ -135,7 +136,7 @@ def overlap_valid(orientation, square, length, matrix, identity):  # Function th
                 return False
         # print("No overlaps detected")
         return True
-    elif orientation == 'Horizontal':
+    elif orientation == 'horizontal':
         for i in range(0, length):
             if matrix[cell[0]][cell[1]+i] in "OI":
                 if identity == "Player":
@@ -170,12 +171,12 @@ def coordinate_converter(square):  # Takes A0 (as a string) and turns it into a 
 def place_friendly_ship(orientation, square, length, matrix, ship_name, player_ship_location_dict):  # Takes all the variables listed and returns a tuple: the first element is a matrix with the ship added, the second is an updated ships_location_dict.
     coords = coordinate_converter(square)
     cells = []
-    if orientation == "Vertical":
+    if orientation == "vertical":
         for i in range(0, length):
             # print("Placing at coordinates ({}, {})".format(coords[0]+i, coords[1]))
             matrix[coords[0]+i][coords[1]] = "O"
             cells.append((coords[0]+i, coords[1]))
-    elif orientation == "Horizontal":
+    elif orientation == "horizontal":
         for i in range(0, length):
             # print("Placing at coordinates ({}, {})".format(coords[0], coords[1]+i))
             matrix[coords[0]][coords[1]+i] = "O"
@@ -190,12 +191,12 @@ def place_friendly_ship(orientation, square, length, matrix, ship_name, player_s
 def place_enemy_ship(orientation, square, length, matrix, ship_name, ai_ship_location_dict):  # Takes all the variables listed and returns a tuple: the first element is a matrix with the ship added, the second is an updated ships_location_dict.
     coords = coordinate_converter(square)
     cells = []
-    if orientation == "Vertical":
+    if orientation == "vertical":
         for i in range(0, length):
             # print("Placing at coordinates ({}, {})".format(coords[0]+i, coords[1]))
             matrix[coords[0]+i][coords[1]] = "I"
             cells.append((coords[0]+i, coords[1]))
-    elif orientation == "Horizontal":
+    elif orientation == "horizontal":
         for i in range(0, length):
             # print("Placing at coordinates ({}, {})".format(coords[0], coords[1]+i))
             matrix[coords[0]][coords[1]+i] = "I"
@@ -208,8 +209,7 @@ def place_enemy_ship(orientation, square, length, matrix, ship_name, ai_ship_loc
 
 
 if __name__ == "__main__":
-    matrix, ai_dict= ship_placement_ai(playerBoard)
-    bad_ideas = "bad_ideas removed. Edit ship_placement_ai."
-    dM.printBoard(matrix)
-    print(ai_dict)
-    print(bad_ideas)
+    player_board, player_dict = ship_placement_player(playerBoard)
+    ai_board, ai_dict = ship_placement_ai(enemyBoard)
+    dM.printBothBoards(player_board, ai_board)
+    dM.printBothBoards(player_board, dM.hideHidden(ai_board))

e30bcb4081adeedfb44bf2b06aec801b7e0e871c
Caspian <caspianmacky@gmail.com>
2019-12-08 23:13:50 -0500

    Fixed wincheck

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def Battleship():
     playerBoard, player_ship_location_dict = ship_placement_player(playerBoard)
     enemyBoard, ai_ship_location_dict = ship_placement_ai(enemyBoard)
     playerFirst = RockPaperScissors()
+    printBothBoards(playerBoard, hideHidden(enemyBoard))
     if playerFirst == True:
-        printBothBoards(playerBoard, hideHidden(enemyBoard))
         playerShot(playerBoard, enemyBoard)
     while check_win(playerBoard, "Player") == False and check_win(enemyBoard, "AI") == False:
         enemyShot(playerBoard, enemyBoard, loopCount)
@@ -36,16 +36,13 @@ def Battleship():
                 cells = player_ship_location_dict[ship]
                 for cell in cells:
                     playerBoard[cell[0]][cell[1]] = 'V'
-                print("Your ship has sunk!")
         playerShot(playerBoard, enemyBoard)
         for ship in ai_ship_location_dict:
             if check_sink(ship, ai_ship_location_dict, enemyBoard) == True:
                 cells = ai_ship_location_dict[ship]
                 for cell in cells:
                     enemyBoard[cell[0]][cell[1]] = 'V'
-
-                print("You sunk an enemy ship!")
-    if check_win(enemyBoard, "AI"):
+    if check_win(playerBoard, "Player"):
         print("Congratulations! You have won at Battleships!")
     else:
         print("You have lost! Better luck next time!")

1d4267b2d773223967c2909cf28c2aa4cde9802a
Caspian <caspianmacky@gmail.com>
2019-12-08 23:20:02 -0500

    print sunk statements

--- main.py
+++ main.py
@@ -36,13 +36,15 @@ def Battleship():
                 cells = player_ship_location_dict[ship]
                 for cell in cells:
                     playerBoard[cell[0]][cell[1]] = 'V'
+                print("The enemy has sunk one of your ships!")
         playerShot(playerBoard, enemyBoard)
         for ship in ai_ship_location_dict:
             if check_sink(ship, ai_ship_location_dict, enemyBoard) == True:
                 cells = ai_ship_location_dict[ship]
                 for cell in cells:
                     enemyBoard[cell[0]][cell[1]] = 'V'
-    if check_win(playerBoard, "Player"):
+                print("You have sunk an enemy ship!")
+    if check_win(enemyBoard, "AI"):
         print("Congratulations! You have won at Battleships!")
     else:
         print("You have lost! Better luck next time!")
